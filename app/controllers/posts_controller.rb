ce4a862d45e928cb459af8cea095f634df401a6d
say-yama <muhaku.124321@gmail.com>
2022-07-13 14:30:04 +0000

    [Update]投稿機能に編集機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,13 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(params[:id])
   end
 
   def destroy

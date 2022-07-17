bdbe3236dc1d15e0d404185560fa6e624943ee39
say-yama <muhaku.124321@gmail.com>
2022-07-17 14:09:21 +0000

    [Update]修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,10 +2,13 @@ class HomesController < ApplicationController
   def top
    @posts = Post.all
    @ranks = Post.group('title').order('count_all DESC').count
-  @movie_ranks = Post.group('title').having('category_id = 1').order('count_all DESC').count
-  @book_ranks = Post.group('title').having('category_id = 2').order('count_all DESC').count
-  @comic_ranks = Post.group('title').having('category_id = 3').order('count_all DESC').count
-  
+#   @movie_ranks = Post.group('title').having('category_id = 1').order('count_all DESC').count
+#   @book_ranks = Post.group('title').having('category_id = 2').order('count_all DESC').count
+#   @comic_ranks = Post.group('title').having('category_id = 3').order('count_all DESC').count
+    @movie_ranks = Post.joins(:category).group(:title).having('count(*) = ?', 1).count
+    @book_ranks = Post.joins(:category).group(:title).having('count(*) = ?', 2).count
+    @comic_ranks = Post.joins(:category).group(:title).having('count(*) = ?', 3).count
+
   end
 
   def about

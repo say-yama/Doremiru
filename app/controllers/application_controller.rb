5c76f40cc65755b467f49977c11573a747f8f88e
say-yama <muhaku.124321@gmail.com>
2022-07-21 13:52:16 +0000

    [Add]バリデーションの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   # 管理者と会員がどちらもログインしてなかったら
-  before_action :authenticate_user!, except: [:top, :about, :search], unless: :admin_signed_in?
+  before_action :authenticate_user!, except: [:top, :about, :search, :show], unless: :admin_signed_in?
   
   # 管理者ログインせずに管理人ページにアクセスした時
   def authenticate_admin
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,10 +2,8 @@ class HomesController < ApplicationController
   def top
    @posts = Post.all.order(created_at: :desc)
    @ranks = Post.group('title').order('count_all DESC').count
-  # 開発環境下
    @book_ranks = Post.books.group('title').order('count_all DESC').count
    @comic_ranks = Post.comics.group('title').order('count_all DESC').count
-  #
   end
 
   def about

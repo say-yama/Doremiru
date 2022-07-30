bee15cb49ec86b99c43cf2162e7e23d9fdf3cdbe
say-yama <muhaku.124321@gmail.com>
2022-07-30 23:34:36 +0000

    [Update]別の人のマイページで削除が表示される不具合を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:favorites]
   def show
     @user = User.find(params[:id])
+    @show_user = current_user
     @posts = @user.posts.order(created_at: :desc).page(params[:page]).per(5) #新着順
   end
 
